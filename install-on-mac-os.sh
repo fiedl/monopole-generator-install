2861183e857a18f25a3cd28da2e3f84862beb35c
Sebastian Fiedlschuster <github@fiedlschuster.de>
2019-09-16 17:11:07 +0200

    try out different install instructions from https://code.icecube.wisc.edu/projects/icecube/ticket/2225#comment:2
    because the current trunk does not compile:
    https://github.com/fiedl/monopole-generator-install/runs/223861175
    
    Maybe, my system-package install is wrong.

--- install-on-mac-os.sh
+++ install-on-mac-os.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# This script provides instructions on how to install the icecube-simulation framework
-# with monopole-generator on macOS.
-# https://github.com/fiedl/monopole-generator-install
-
-# In order to separate out several build steps with github actions, I'm using the
-# `BUILD_STEP` enviornment variable. When using these instructions on your local machine,
-# you don't need the `BUILD_STEP` environment variable.
-
+# Try install instrcutions from
+# https://code.icecube.wisc.edu/projects/icecube/ticket/2225#comment:2
 
 # Homebrew paths
 export PATH=/usr/local/bin:$PATH
@@ -16,41 +10,25 @@ export PATH=/usr/local/sbin:$PATH
 # Python path when installed via homebrew
 export PATH=/usr/local/opt/python/libexec/bin:$PATH
 
+# Install Homebrew package manager (http://brew.sh):
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-if [[ ! -z "$BUILD_STEP" ]] && [[ $BUILD_STEP = "SYSTEM_PACKAGES" ]]; then
+brew list cmake || brew install cmake
+brew install boost boost-python3 cdk qt libarchive wget doxygen cfitsio hdf5 nlopt gsl minuit2 suite-sparse zstd healpix
 
-  # Install Homebrew package manager (http://brew.sh):
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#install brews writen by icecube
+brew tap IceCube-SPNO/homebrew-icecube
+brew install multinest pal sprng2 cppzmq
 
-  # Install python 3
-  brew info python
-  brew list python || brew install python
+#create virtual envionment
+python3 -m venv ~/py3/
+#activate virtual envionment
+source ~/py3/bin/activate
 
-  # Install boost with python bindings
-  brew info boost
-  brew install fiedl/homebrew-icecube/boost@1.69
-  brew info boost-python
-  brew info boost-python3
-  brew install fiedl/homebrew-icecube/boost-python3@1.69
+#install python packages
+pip install --upgrade pip
+pip install urwid sphinx ipython qtconsole tables numpy scipy matplotlib
 
-  # Install python packages
-  pip install numpy
-  pip install scipy
-
-  # Install packages needed for building icecube-simulation
-  brew list cmake || brew install cmake
-  brew install gsl cfitsio
-
-  # Install qt5 needed for steamshovel event display viewer
-  brew info qt
-  brew install qt
-  #brew install pyqt
-
-  ## Install packages from icecube sources, e.g. random number generator
-  #brew tap IceCube-SPNO/homebrew-icecube
-  #brew install pal sprng2 cppzmq
-
-fi
 
 # This is where the icecube software will live
 export ICECUBE_ROOT="$HOME/icecube/software"
@@ -72,88 +50,58 @@ export ICECUBE_ROOT="$HOME/icecube/software"
 export ICESIM_ROOT=$ICECUBE_ROOT/icecube-simulation-$RELEASE
 export ICESIM=$ICESIM_ROOT/debug_build
 
-if [[ ! -z "$BUILD_STEP" ]] && [[ $BUILD_STEP = "ICECUBE_SIMULATION_BUILD" ]]; then
+# Get icecube-simulation code from svn repository
+mkdir -p $ICESIM_ROOT
+if [ ! -d $ICESIM_ROOT/src ]; then
+  if [[ -z $SVN_ICECUBE_USERNAME ]]; then
+    source .secrets.sh
+  fi
+  if [[ $RELEASE = "trunk" ]]; then
+    svn --username $SVN_ICECUBE_USERNAME --password $SVN_ICECUBE_PASSWORD co $SVN/meta-projects/simulation/trunk/ $ICESIM_ROOT/src
+  else
+    svn --username $SVN_ICECUBE_USERNAME --password $SVN_ICECUBE_PASSWORD co $SVN/meta-projects/simulation/releases/$RELEASE/ $ICESIM_ROOT/src
+  fi
+fi
 
-  # Get icecube-simulation code from svn repository
-  mkdir -p $ICESIM_ROOT
-  if [ ! -d $ICESIM_ROOT/src ]; then
-    if [[ -z $SVN_ICECUBE_USERNAME ]]; then
-      source .secrets.sh
-    fi
-    if [[ $RELEASE = "trunk" ]]; then
-      svn --username $SVN_ICECUBE_USERNAME --password $SVN_ICECUBE_PASSWORD co $SVN/meta-projects/simulation/trunk/ $ICESIM_ROOT/src
-    else
-      svn --username $SVN_ICECUBE_USERNAME --password $SVN_ICECUBE_PASSWORD co $SVN/meta-projects/simulation/releases/$RELEASE/ $ICESIM_ROOT/src
-    fi
+# Exclude projects if requested by environment variable,
+# which is used on travis to avoid the execution-time limit
+if [[ ! -z $EXCLUDE_PROJECTS ]]; then
+  for project in $EXCLUDE_PROJECTS; do
+    rm -rf $ICESIM_ROOT/src/$project
+  done
+fi
+
+if [[ $RELEASE = "V06-01-01" ]]; then
+
+  # Patch cmake file to find pymalloc version of python installed by homebrew
+  # https://github.com/fiedl/hole-ice-install/issues/1
+  patch --force $ICESIM_ROOT/src/cmake/tools/python.cmake < ./patches/python.cmake.patch
+
+  # Patch muongun pybindings to add missing static cast
+  # https://github.com/fiedl/hole-ice-install/issues/2
+  if [[ -d $ICESIM_ROOT/src/MuonGun ]]; then
+    patch --force $ICESIM_ROOT/src/MuonGun/private/pybindings/histogram.cxx < ./patches/muongun-histogram.cxx.patch
   fi
 
-  # Exclude projects if requested by environment variable,
-  # which is used on travis to avoid the execution-time limit
-  if [[ ! -z $EXCLUDE_PROJECTS ]]; then
-    for project in $EXCLUDE_PROJECTS; do
-      rm -rf $ICESIM_ROOT/src/$project
-    done
-  fi
-
-  if [[ $RELEASE = "V06-01-01" ]]; then
-
-    # Patch cmake file to find pymalloc version of python installed by homebrew
-    # https://github.com/fiedl/hole-ice-install/issues/1
-    patch --force $ICESIM_ROOT/src/cmake/tools/python.cmake < ./patches/python.cmake.patch
-
-    # Patch muongun pybindings to add missing static cast
-    # https://github.com/fiedl/hole-ice-install/issues/2
-    if [[ -d $ICESIM_ROOT/src/MuonGun ]]; then
-      patch --force $ICESIM_ROOT/src/MuonGun/private/pybindings/histogram.cxx < ./patches/muongun-histogram.cxx.patch
-    fi
-
-    # Patch cmake file to drop requirement of the boost_signals library,
-    # which has been dropped in boost 1.69
-    # https://github.com/fiedl/hole-ice-install/issues/3
-    # https://code.icecube.wisc.edu/projects/icecube/ticket/2232
-    patch --force $ICESIM_ROOT/src/cmake/tools/boost.cmake < ./patches/boost.cmake.patch
-
-  fi
-
-  # Build the release (debug)
-  mkdir -p $ICESIM_ROOT/debug_build
-  cd $ICESIM_ROOT/debug_build
-  cmake -D CMAKE_BUILD_TYPE=Debug -D SYSTEM_PACKAGES=true -D CMAKE_BUILD_TYPE:STRING=Debug ../src
-  # source ./env-shell.sh  # <--- is this needed here?
-  make -j 6
-
-  # # Build the release
-  # mkdir -p $ICESIM_ROOT/build
-  # cd $ICESIM_ROOT/build
-  # cmake -D CMAKE_BUILD_TYPE=Release -D SYSTEM_PACKAGES=true -D CMAKE_BUILD_TYPE:STRING=Release ../src
-  # ./env-shell.sh
-  # make -j 2
+  # Patch cmake file to drop requirement of the boost_signals library,
+  # which has been dropped in boost 1.69
+  # https://github.com/fiedl/hole-ice-install/issues/3
+  # https://code.icecube.wisc.edu/projects/icecube/ticket/2232
+  patch --force $ICESIM_ROOT/src/cmake/tools/boost.cmake < ./patches/boost.cmake.patch
 
 fi
-if [[ ! -z "$BUILD_STEP" ]] && [[ $BUILD_STEP = "MONOPOLE_GENERATOR_BUILD" ]]; then
 
-  # Get the monopole-generator code
-  if [ ! -d $ICESIM_ROOT/src/monopole-generator ]; then
-    if [[ -z $SVN_ICECUBE_USERNAME ]]; then
-      source .secrets.sh
-    fi
-    svn --username $SVN_ICECUBE_USERNAME --password $SVN_ICECUBE_PASSWORD co $SVN/projects/monopole-generator/trunk/ $ICESIM_ROOT/src/monopole-generator
-    cd $ICESIM_ROOT/debug_build
-    cmake -D CMAKE_BUILD_TYPE=Debug -D SYSTEM_PACKAGES=true -D CMAKE_BUILD_TYPE:STRING=Debug ../src
-    make
-  fi
+# Build the release (debug)
+mkdir -p $ICESIM_ROOT/debug_build
+cd $ICESIM_ROOT/debug_build
+cmake -D CMAKE_BUILD_TYPE=Debug -D SYSTEM_PACKAGES=true -D CMAKE_BUILD_TYPE:STRING=Debug ../src
+# source ./env-shell.sh  # <--- is this needed here?
+make -j 6
 
-fi
-if [[ ! -z "$BUILD_STEP" ]] && [[ $BUILD_STEP = "BUILD_TEST_BINS" ]]; then
-
-  cd $ICESIM_ROOT/debug_build
-  make test-bins
-
-fi
-if [[ ! -z "$BUILD_STEP" ]] && [[ $BUILD_STEP = "MONOPOLE_GENERATOR_TESTS" ]]; then
-
-  cd $ICESIM_ROOT/debug_build
-  bin/monopole-generator-test -a
-
-fi
+# # Build the release
+# mkdir -p $ICESIM_ROOT/build
+# cd $ICESIM_ROOT/build
+# cmake -D CMAKE_BUILD_TYPE=Release -D SYSTEM_PACKAGES=true -D CMAKE_BUILD_TYPE:STRING=Release ../src
+# ./env-shell.sh
+# make -j 2
 
